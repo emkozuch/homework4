b4f0b9da98bbf7d7342de3823ca317b22b6ca8c2
emkozuch <emkozuch@gmail.com>
2018-12-10 20:06:26 +0100

    made table

--- script.js
+++ script.js
@@ -1,41 +1,54 @@
-var employees = [
-    {
-        firstName: 'John',
-        lastName: 'Doe',
-        age: 31,
-        sex: 'male',
-        employed: true
-    },
-    {
-        firstName: 'Amelia',
-        lastName: 'Clark',
-        age: 22,
-        sex: 'female',
-        employed: true
-    },
-    {
-        firstName: 'Jack',
-        lastName: 'Jones',
-        age: 21,
-        sex: 'male',
-        employed: false
-    },
-    {
-        firstName: 'Olivia',
-        lastName: 'Wilde',
-        age: 31,
-        sex: 'female',
-        employed: true
-    },
-]
-
-    function createEmployee(firstName, lastName, age, sex, employed){
-        return{
+function createEmployee(firstName, lastName, age, sex, employed) {
+    return {
         firstName: firstName,
         lastName: lastName,
         age: age,
         sex: sex,
-        employed: employed,
-
-    };
+        employed: employed
+    }
 }
+
+
+var employees = [];
+employees.push(createEmployee('John', 'Doe', 31, 'male', true))
+employees.push(createEmployee('Amelia', 'Clark', 22, 'female', true))
+employees.push(createEmployee('Jack', 'Jones', 21, 'male', false))
+employees.push(createEmployee('Olivia', 'Wilde', 31, 'female', true))
+
+
+
+function renderEmployees(employees) {
+    
+    var employeesDOM = document.getElementById('table-container');
+    var tableDOM = '<table class="table">\n\
+<thead>\n\
+<tr>\n\
+<th scope="col">Index</th>\n\
+<th scope="col">First Name</th>\n\
+<th scope="col">Last Name</th>\n\
+<th scope="col">Age</th>\n\
+<th scope="col">Sex</th>\n\
+<th scope="col">Employed</th>\n\
+</tr>\n\
+</thead>';
+
+    employees.forEach(function (employee) {
+      
+        tableDOM += '<tr>\n\
+<td>' + employee.index + '</td>\n\
+<td>' + employee.firstName + '</td>\n\
+<td>' + employee.lastName + '</td>\n\
+<td>' + employee.age + '</td>\n\
+<td>' + employee.sex + '</td>\n\
+<td>' + employee.employed + '</td>\n\
+</tr>';
+    });
+
+    tableDOM += '</tbody>\n\
+</table>';
+
+    employeesDOM.innerHTML = tableDOM;
+}
+
+
+renderEmployees(employees);
